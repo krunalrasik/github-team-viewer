84351c04cc3ac71ecffa9d078e8a0dfa97547832
vinit Kumar <vinit.kumar@changer.nl>
2014-03-16 15:14:12 +0530

    fix UI and UX of the app

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,7 @@
 /* Controllers */
 function AppCtrl($scope, $http, $log) {
+  $scope.loading = true;
+  $scope.finding = false;
   $scope.organization = 'github';
   var reset = function () {
     $scope.members = [];
@@ -13,21 +15,24 @@ function AppCtrl($scope, $http, $log) {
 
   $scope.getMembers = function (organization) {
     reset();
-    $log.log("Wanna know about " + organization + ", huh?");
+    $log.log("Getting " + organization + ", for you, hold tight!");
     $http.jsonp("https://api.github.com/orgs/" + organization + "/members?callback=JSON_CALLBACK").success(function (data) {
       $scope.members = data.data;
+      $scope.loading = false;
+      $scope.finding = true;
     });
   };
 
   $scope.getUser = function (user) {
-    $log.log("Getting the latest info for");
+    $log.log("Getting " + user + "'s data.");
     $http.jsonp("https://api.github.com/users/" + user + "?callback=JSON_CALLBACK").success(function (data) {
       $scope.user = data.data;
+      $scope.loading = false;
     });
   };
 
   $scope.getRepos = function (user) {
-    $log.log("Wanna get repos from " + user + ", huh?");
+    $log.log("Fetching projects of " + user);
     $http.jsonp("https://api.github.com/users/" + user + "/repos?callback=JSON_CALLBACK").success(function (data) {
       $scope.repos = data.data;
     });
