888ae7565e0fe2a386af909f77bda80f2fd9e016
vinit Kumar <vinit.kumar@changer.nl>
2014-03-16 15:18:30 +0530

    push new UI to github pages

--- js/controllers.js
+++ js/controllers.js
@@ -1,48 +1,49 @@
-'use strict';
-
 /* Controllers */
-
-
 function AppCtrl($scope, $http, $log) {
+  $scope.loading = true;
+  $scope.finding = false;
   $scope.organization = 'github';
-  var reset = function() {
+  var reset = function () {
     $scope.members = [];
     $scope.repos = [];
     $scope.repos = [];
     $scope.commits = [];
     $scope.current_member = null;
     $scope.current_repo = null;
-  }
+  };
   reset();
 
-  $scope.getMembers = function(organization){
+  $scope.getMembers = function (organization) {
     reset();
-    $log.log("Wanna know about " + organization + ", huh?");
-    $http.jsonp("https://api.github.com/orgs/" + organization + "/members?callback=JSON_CALLBACK").success(function(data){
+    $log.log("Getting " + organization + ", for you, hold tight!");
+    $http.jsonp("https://api.github.com/orgs/" + organization + "/members?callback=JSON_CALLBACK").success(function (data) {
       $scope.members = data.data;
+      $scope.loading = false;
+      $scope.finding = true;
     });
-  }
+  };
 
   $scope.getUser = function (user) {
-    $log.log("Getting the latest info for");
-    $http.jsonp("https://api.github.com/users/"+user+"?callback=JSON_CALLBACK").success(function(data) {
+    $log.log("Getting " + user + "'s data.");
+    $http.jsonp("https://api.github.com/users/" + user + "?callback=JSON_CALLBACK").success(function (data) {
       $scope.user = data.data;
+      $scope.loading = false;
     });
-  }
+  };
 
-  $scope.getRepos = function(user){
-    $log.log("Wanna get repos from " + user + ", huh?");
-    $http.jsonp("https://api.github.com/users/" + user + "/repos?callback=JSON_CALLBACK").success(function(data){
+  $scope.getRepos = function (user) {
+    $log.log("Fetching projects of " + user);
+    $http.jsonp("https://api.github.com/users/" + user + "/repos?callback=JSON_CALLBACK").success(function (data) {
       $scope.repos = data.data;
     });
-  }
+  };
 
-  $scope.getCommits = function(repo){
+  $scope.getCommits = function (repo) {
     $scope.current_repo = repo;
     $log.log("Wanna get commits from " + repo + ", huh?");
-    $http.jsonp("https://api.github.com/repos/" + $scope.current_member + "/" + repo + "/commits?callback=JSON_CALLBACK").success(function(data){
+    $http.jsonp("https://api.github.com/repos/" + $scope.current_member + "/" + repo + "/commits?callback=JSON_CALLBACK").success(function (data) {
       $scope.commits = data.data;
     });
-  }
+  };
 }
 //MyCtrl1.$inject = [];
